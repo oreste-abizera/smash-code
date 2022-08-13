4323a169a06d750f6e9e516c7f604f9193d2eae4
oreste-abizera <oresteabizera11@gmail.com>
2022-08-14 01:29:22 +0200

    feat: hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="bg-[rgb(31,31,31)] w-full min-h-screen"></div>
+      <div className="bg-[rgb(31,31,31)] w-full min-h-screen px-4 lg:px-8 py-10">
+        <Hero />
+      </div>
       <Footer />
     </>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-wrap py-[8rem]">
+      <div className="w-full lg:w-2/3">
+        <div className="text-white text-[4rem]">
+          <p>What You Can Think</p>
+          <p>We Can Develop It</p>
+          <p>For You</p>
+        </div>
+        <div className="h-[0.7px] w-[30%] bg-[#1aaca2] my-[15px]"></div>
+        <p className="text-[1.2rem] text-white my-[30px]">
+          We create future-ready websites &amp; web apps via innovative digital
+          solutions so that you can focus on your core business.
+        </p>
+        <button className="primary-btn">Let's talk</button>
+      </div>
+      <div className="w-full lg:w-1/3"></div>
+    </div>
+  );
+};
+
+export default Hero;
