8b1ac7f327669701d2d6fee7ff8bc39b797c0c05
oreste-abizera <oresteabizera11@gmail.com>
2022-08-17 22:07:40 +0200

    chore: home hero images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import heroImage1 from "../assets/images/web/hero1.png";
+import heroImage2 from "../assets/images/web/hero2.png";
+import heroImage3 from "../assets/images/web/hero3.png";
 
 const Hero = () => {
+  let x;
+  const images = [heroImage3, heroImage2, heroImage1];
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImage((currentImage) => {
+      let newImage = currentImage >= images.length - 1 ? 0 : currentImage + 1;
+      return newImage;
+    });
+    if (x) x.refresh();
+  };
+
+  useEffect(() => {
+    x = setInterval(nextImage, 5000);
+    return () => clearInterval(x);
+  }, []);
   const navigate = useNavigate();
   return (
     <div className="flex flex-wrap py-[8rem]">
@@ -22,7 +40,12 @@ const Hero = () => {
         </button>
       </div>
       <div className="w-full lg:w-1/3 pt-[4rem] flex items-center justify-center">
-        <img src={heroImage1} alt="" className="w-full"></img>
+        <img
+          src={images[currentImage]}
+          alt=""
+          className="w-full"
+          onClick={nextImage}
+        ></img>
       </div>
     </div>
   );
