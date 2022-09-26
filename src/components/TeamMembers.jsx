372364c4279a158d180ab01061c173d33fd9150b
oreste-abizera <oresteabizera11@gmail.com>
2022-09-26 21:46:17 +0200

    chore: adding developers

--- src/components/TeamMembers.jsx
+++ src/components/TeamMembers.jsx
@@ -4,64 +4,64 @@ import TeamMember from "./TeamMember";
 const TeamMembers = (props) => {
   const members = [
     {
-      name: "Nate fuller",
+      name: "Nate Fuller",
       position: "CEO & Founder",
       image:
         "https://res.cloudinary.com/dkndyyvmn/image/upload/v1664200028/4e6b2743-8b87-4d35-a995-ebbe689510a0_ux3bvv.jpg",
       isExpanded: true,
     },
     {
-      name: "NTWALI OSEE",
+      name: "Ntwali Osee",
       position: "Software Engineer",
       image:
         "https://res.cloudinary.com/dkndyyvmn/image/upload/v1664200247/ce5acdf1-4ea5-4a4c-9082-ce521ca464e5_d9zqqt.jpg",
     },
     {
-      name: "ABIZERA Oreste",
-      position: "Fullstack Developer",
+      name: "Oreste Abizera",
+      position: "Senior Software Engineer",
       image:
         "https://res.cloudinary.com/dkndyyvmn/image/upload/v1664200797/T5E22CTMW-U03V01LLPT4-3415dd4eeb48-512_i8bjtn.jpg",
       hideAfter: true,
     },
     {
-      name: "valens NIYONSENGA",
-      position: "Developer",
+      name: "Valens Niyonsenga",
+      position: "Software Developer",
       image:
         "https://res.cloudinary.com/dkndyyvmn/image/upload/v1664201177/c97d82cf-ba90-4f9d-9dd6-6db045734f63_mthzec.jpg",
       showBefore: true,
     },
     {
-      name: "NTWALI OSEE",
-      position: "Software Engineer",
+      name: "Wilson Kamali",
+      position: "Software Developer",
       image:
-        "https://res.cloudinary.com/dkndyyvmn/image/upload/v1664200237/467033c5-d094-40f0-993e-3d538d3f13e8_un1pce.jpg",
+        "https://res.cloudinary.com/dkndyyvmn/image/upload/v1664220626/7caece85-ebf2-45a3-9e02-7c15461c6c07_shzsgm.jpg",
     },
     {
-      name: "ABIZERA Oreste",
-      position: "Fullstack Developer",
+      name: "Hubert Munezero",
+      position: "Frontend developer and UI IX designer",
       image:
-        "https://res.cloudinary.com/dkndyyvmn/image/upload/v1664201107/T043BG5SCR5-U043KF5598E-5aa2a6f547b8-512_sl65kg.png",
+        "https://res.cloudinary.com/dkndyyvmn/image/upload/v1664220642/f7b50543-a582-4c8a-843b-79054bb3474b_wjtcbz.jpg",
       hideAfter: true,
     },
     {
-      name: "valens NIYONSENGA",
-      position: "Developer",
+      name: "Valens Niyonsenga",
+      position: "Software Developer",
       image:
         "https://res.cloudinary.com/dkndyyvmn/image/upload/v1664201177/c97d82cf-ba90-4f9d-9dd6-6db045734f63_mthzec.jpg",
 
       showBefore: true,
     },
     {
-      name: "NTWALI OSEE",
+      name: "Ntwali Osee",
       position: "Software Engineer",
       image:
-        "https://res.cloudinary.com/dkndyyvmn/image/upload/v1664200247/ce5acdf1-4ea5-4a4c-9082-ce521ca464e5_d9zqqt.jpg",
+        "https://res.cloudinary.com/dkndyyvmn/image/upload/v1664200237/467033c5-d094-40f0-993e-3d538d3f13e8_un1pce.jpg",
     },
     {
-      name: "ABIZERA Oreste",
-      position: "Fullstack Developer",
+      name: "Oreste Abizera",
+      position: "Senior Software Engineer",
       image:
-        "https://res.cloudinary.com/dkndyyvmn/image/upload/v1664200797/T5E22CTMW-U03V01LLPT4-3415dd4eeb48-512_i8bjtn.jpg",
+        "https://res.cloudinary.com/dkndyyvmn/image/upload/v1664201107/T043BG5SCR5-U043KF5598E-5aa2a6f547b8-512_sl65kg.png",
       hideAfter: true,
     },
   ];
